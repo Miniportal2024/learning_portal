6a7e3c9f109454e1c44fbbf4afdea48df991508d
johns <icoj462@gmail.com>
2023-12-04 23:54:24 +0800

    mild edit

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -145,6 +145,7 @@
                                         <div class="content">
                                             <h3 data-animation="animated slideInLeft">Reach you career</h3>
                                             <h1 data-animation="animated slideInUp">Learn from best online training course</h1>
+                                            <a data-animation="animated slideInUp" class="btn btn-theme effect btn-md" href="/about-us">Lear More</a>
                                             <a data-animation="animated slideInUp" class="btn btn-theme effect btn-md" href="/courses">View Courses</a>
                                         </div>
                                     </div>
